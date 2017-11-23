576d56ece5b9a69920e47dca21df9d297444c523
Matthias Wimmer <m@tthias.eu>
2017-11-23 14:46:40 +0100

    HTTP-Echo-Server

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,57 @@ extern crate hyper;
 extern crate futures;
 
 use futures::future::Future;
+use futures::Stream;
+use hyper::{Body, Chunk, Method, StatusCode};
 use hyper::header::ContentLength;
 use hyper::header::ContentType;
 use hyper::server::{Http, Request, Response, Service};
+use std::ascii::AsciiExt;
 
-struct HelloWorld;
+struct Echo;
 
-const PHRASE: &'static str = "你好！";
-
-impl Service for HelloWorld {
+impl Service for Echo {
     // boilerplate hooking up hyper's server types
     type Request = Request;
-    type Response = Response;
+    type Response = Response<Box<Stream<Item=Chunk, Error=Self::Error>>>;
     type Error = hyper::Error;
     // The future representing the eventual Pesponse your call will
     // resolve to. This can change to whatever Future you need.
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
-    fn call(&self, _req: Request) -> Self::Future {
-        // We're currently igonoring the Request
-        // And returning an 'ok' Future, which means it's ready
-        // immediately, and build a Response with the 'PHRASE' body.
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_header(ContentType::plaintext())
-                .with_body(PHRASE)))
+    fn call(&self, req: Request) -> Self::Future {
+        let mut response = Response::new();
+
+        match (req.method(), req.path()) {
+            (&Method::Get, "/") => {
+                let body: Box<Stream<Item=_, Error=_>> =
+                    Box::new(Body::from("Try POSTing to /echo!"));
+                response.set_body(body);
+            },
+            (&Method::Post, "/echo") => {
+                let mapping =
+                    req.body().map(to_uppercase as fn(Chunk) -> Chunk);
+                let body: Box<Stream<Item=_, Error=_>> = Box::new(mapping);
+                response.set_body(body);
+            },
+            _ => {
+                response.set_status(StatusCode::NotFound);
+            }
+        }
+
+        Box::new(futures::future::ok(response))
     }
+}
+
+fn to_uppercase(chunk: Chunk) -> Chunk {
+    Chunk::from(
+        chunk.iter()
+            .map(|byte| byte.to_ascii_uppercase())
+            .collect::<Vec<u8>>())
 }
 
 fn main() {
     let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
     server.run().unwrap();
 }
